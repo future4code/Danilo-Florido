816a0090442195cdd07de55e9da4c76c8803b4fd
NiloFlorido <danilo1234@gmail.com>
2021-10-07 19:37:55 -0300

    Exercício 6 - Revisão JS

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -49,6 +49,17 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2
+  const booleano4 = !booleano3
+  const respostaVF = [booleano1 && booleano2 && !booleano4,
+  (booleano1 && booleano2) || !booleano3,
+  (booleano2 || booleano3) && (booleano4 || booleano1),
+  !(booleano2 && booleano3) || !(booleano1 && booleano3),
+  !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)]
+
+  return respostaVF
 
 }
 
