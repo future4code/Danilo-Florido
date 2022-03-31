23a1f8a9bbd435b08a7ab67f6f353c6dbe617934
NiloFlorido <danilo1234@gmail.com>
2022-03-31 19:21:42 -0300

    Aprofundamento Express

---
+++ aprofundamento-express/src/index.ts
@@ -0,0 +1,54 @@
+import express from "express";
+
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});;
+
+
+//Exercício 1
+app.get("/ping", (req, res) => {          
+		res.send("Pong! 🏓")
+}
+
+
+//Exercício 2
+type ToDo={
+	userId:number,
+	id:number,
+	title:string,
+	completed:boolean
+}
+
+
+//Exercício 3
+const Afazeres:ToDo[]=[
+	{
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false
+  },
+]
