5cad16da48bd6cc3aa7cdcf693941ffcb301842b
NiloFlorido <danilo1234@gmail.com>
2022-04-05 20:45:34 -0300

    Fluxo de Dados Express

---
+++ fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,25 @@
+export type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const products: Product[] = [
+    {
+        id: "666-bue2-7777",
+        name: "Mouse",
+        price: 40
+    },
+    {
+        id: "777-fue3-5656",
+        name: "Teclado",
+        price: 50
+    },
+    {
+        id: "888-gue4-7878",
+        name: "Headset",
+        price: 100
+    }
+]
+
+
